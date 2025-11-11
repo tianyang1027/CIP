9d7f4d5866107b9bce7198e2d2d36b525ab112a5
Tim Y Tian <574713572@qq.com>
2025-11-11 10:44:22 +0800

    Remove invalid code (#10)
    * update
    
    * remove useless code
    
    * Add validate extracted steps
    
    * Add streamlit app
    
    * Fix iframe content not load issue
    
    * update
    
    * Updated README.md
    
    * update code
    
    ---------
    
    Co-authored-by: Yang Tian <v-yantian@microsoft.com>

--- utils.py
+++
@@ -1,9 +0,0 @@
-import pytesseract
-from PIL import Image
-import cv2
-
-# OCR 识别文字
-def ocr_image(frame):
-    img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    text = pytesseract.image_to_string(img)
-    return text.strip()
